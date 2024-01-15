e34de21090f4ca86673f64384aa8057189e67537
javase <java_03@126.com>
2024-01-15 18:05:57 +0800

    QueryWrapper

---
+++ src/test/java/com/hua/mp/QueryDemo.java
@@ -0,0 +1,30 @@
+package com.hua.mp;
+
+import java.util.List;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hua.mp.dao.entity.User;
+import com.hua.mp.dao.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class QueryDemo {
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+	void queryUser() {
+		QueryWrapper<User> wrapper = new QueryWrapper<>();
+		wrapper.like("name", "飞")
+				.gt("age", 17)
+				.isNotNull("email");
+		final List<User> users = userMapper.selectList(wrapper);
+		users.forEach(user -> log.info("{}", user));
+	}
+}
