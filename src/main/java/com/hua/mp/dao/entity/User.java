a7d9a1b9025603b4d12d0dab3231bb1b599b28ae
javase <java_03@126.com>
2024-01-15 17:27:11 +0800

    自增主键

--- src/main/java/com/hua/mp/dao/entity/User.java
+++ src/main/java/com/hua/mp/dao/entity/User.java
@@ -1,5 +1,7 @@
 package com.hua.mp.dao.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
@@ -8,6 +10,7 @@ import lombok.ToString;
 @Data
 @Builder
 public class User {
+	@TableId(type = IdType.AUTO)
 	private Long id;
 
 	private String name;
